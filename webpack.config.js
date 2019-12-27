ce8cf965df80a618cb247489fcd331045577c5b2
J Lopes <joao.lopes@showpad.com>
2019-12-27 18:18:25 +0000

    renaming output folder to docs

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = (env, argv) => {
     },
     output: {
       filename: '[name].js',
-      path: path.resolve(__dirname, 'public'),
+      path: path.resolve(__dirname, 'docs'),
       publicPath: '/'
     },
     resolve: {
